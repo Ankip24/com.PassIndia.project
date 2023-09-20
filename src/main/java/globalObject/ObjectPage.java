8b71ad813f3e5fdc5cc0df7a89d0544acc5bf32d
Amaze <Amaze@DESKTOP-C92595F>
2023-09-20 19:28:51 +0530

    Helllo

--- src/main/java/globalObject/ObjectPage.java
+++ src/main/java/globalObject/ObjectPage.java
@@ -9,12 +9,19 @@ import com.aventstack.extentreports.ExtentTest;
 public class ObjectPage {
 	// Objetct 
 	
+	/* public sataic void driver This variable is declared in a class (DriverInit) and initialized 
+	 in "@BeforeClass" of various Test Plans(Test Classes). The initialized variable (driver)
+	  will be used across the project 
+	in various re-usable functions and @test methods.*/
 	public static WebDriver driver;
+	
+	// when we get on HTML page report which one connect through the Objecttest which extends with ExtentTest
 	public static ExtentTest objextenttest;
 	
 	public void setWebDriver(WebDriver driver) {
 		
-		ObjectPage.driver=driver;
+		
+		ObjectPage.driver=driver;     
 	}
 	public static WebDriver getWebDriver() {
 		return driver;
